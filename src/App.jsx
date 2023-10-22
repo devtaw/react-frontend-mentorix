264b6febea4948dec9b8c4a75e4781b5338aa9d6
Taw Simões <tawm.dev@gmail.com>
2023-10-22 18:44:28 -0300

    :wrench: chore: Importa estilos do Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export function App() {
   return (
