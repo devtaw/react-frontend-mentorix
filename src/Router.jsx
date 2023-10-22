28b21d2e0b7cc3ba3dc19121450ec4cc9624c60d
Taw Simões <tawm.dev@gmail.com>
2023-10-22 17:51:23 -0300

    :bug: fix: Corrige importação da página Home

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
+import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
 import { Cadastro } from "./pages/Cadastro/Cadastro";
 import { ListaMentores } from "./pages/ListaMentores/ListaMentores";
