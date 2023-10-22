4931c4f62401de0efa951952558e5a3247f0fce3
Taw Simões <tawm.dev@gmail.com>
2023-10-22 18:20:26 -0300

    :lipstick: feat: Configuração de fontes

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -6,26 +6,27 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
   :root {
     --header-hight: 6.75rem;
   }
+
   body, input, textarea, button {
-    /* font-family: 'Roboto', sans-serif; */
+    font-family: 'Montserrat', sans-serif;
     font-weight: 400;
     font-size: 1rem;
     background: ${(props) => props.theme.background};
   }
+
+  button {
+    line-height: 0;
+  }
+
   // titles
-  h1, h2, h3, h4 {
-    /* font-family: 'Baloo 2'; */
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Open Sans', sans-serif;
     font-style: normal;
     line-height: 130%;
     font-weight: 700;
-  }
-  h4 {
-    font-size: 18px;
-  }
-  button {
-    line-height: 0;
   }
 `;
