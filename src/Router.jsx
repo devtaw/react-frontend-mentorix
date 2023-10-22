539c43a7b35494698de28cf5938e760f6cbd8760
Taw Simões <tawm.dev@gmail.com>
2023-10-22 18:53:57 -0300

    :bulb: docs: Adiciona comentários explicativos ao código

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,6 +5,20 @@ import { Cadastro } from "./pages/Cadastro/Cadastro";
 import { ListaMentores } from "./pages/ListaMentores/ListaMentores";
 import { AreaMentor } from "./pages/AreaMentor/AreaMentor";
 
+/**
+ * No geral, esse código configura o roteamento para um aplicativo React de várias páginas,
+ * permitindo ao usuário navegar entre diferentes páginas com base no caminho da URL.
+ *
+ * Cada componente `Route` possui uma propriedade `path` que especifica o caminho da URL
+ *  que deve acionar a renderização da propriedade `element` do componente.
+ *
+ * A propriedade `element` é definida como um componente diferente para cada `Route`.
+ * A primeira `Route` tem um `path` de `/` e um `element` de `<Home />`. Isso significa que
+ *  quando o caminho da URL for `/`, o componente `Home` será renderizado.
+ * Da mesma forma, a segunda `Rota` tem um `caminho` de `/login` e um `elemento` de `<Login />`,
+ * então quando o caminho da URL for `/login`, o componente `Login` será renderizado.
+ */
+
 export function Router() {
   return (
     <Routes>
--- src/common/typography.js
+++ src/common/typography.js
@@ -1,5 +1,18 @@
 import styled, { css } from "styled-components";
 
+/**
+ * O componente Title é um elemento h2 estilizado que define a propriedade font-family como "Open Sans",
+ * uma fonte sem serifa, e o tamanho da fonte (font-size) com base na propriedade props.variant,
+ * que é padrão para "title-sm" se não for especificada.
+ * A propriedade font-weight é definida como 700 se a propriedade bold for verdadeira e 400 caso contrário.
+ * A propriedade color é definida com base na propriedade props.color, que é padrão para "gray-900" se não for especificada.
+ *
+ * exemplo de uso:
+ * <Title variant="title-lg" color="gray-700" bold>
+ *   Título
+ * </Title>
+ */
+
 export const Title = styled.h2`
   font-family: "Open Sans", sans-serif;
   font-style: normal;
@@ -7,6 +20,19 @@ export const Title = styled.h2`
   font-weight: ${(props) => (props.bold ? 700 : 400)};
   color: ${(props) => props.theme[props.color || "gray-900"]};
 `;
+
+/**
+ * O componente Text é um elemento p estilizado que define a propriedade font-family como "Montserrat", outra fonte sem serifa,
+ * e o tamanho da fonte (font-size) com base na propriedade props.variant, que é padrão para "body-md" se não for especificada.
+ * A propriedade font-weight é definida como "bold" se a propriedade bold for verdadeira e 400 caso contrário.
+ * A propriedade line-height é definida como 1.3, e a propriedade color é definida com base na propriedade props.color,
+ * que é padrão para "base-text" se não for especificada.
+ *
+ * exemplo de uso:
+ * <Text variant="body-lg" color="gray-700">
+ *  Texto
+ * </Text>
+ */
 
 export const Text = styled.p`
   font-family: 'Montserrat', sans-serif;
