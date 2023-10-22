9f4dec95abf813df959836be5560e32ba806904f
Taw Simões <tawm.dev@gmail.com>
2023-10-22 18:33:27 -0300

    :lipstick: feat: Configura tema do projeto

---
+++ src/styles/themes/default.js
@@ -0,0 +1,30 @@
+export const defaultTheme = {
+  white: "#FFF",
+  background: " #FAFAFA",
+
+  "gray-100": "#F5F5F5",
+  "gray-300": "#E0E0E0",
+  "gray-500": "#9E9E9E",
+  "gray-700": "#616161",
+  "gray-900": "#212121",
+
+  "primary-300": "#EBE5F9",
+  "primary-500": "#8047F8",
+  "primary-700": "#4B2995",
+
+  "accent-300": "#F1E9C9",
+  "accent-500": "#DBAC2C",
+  "accent-700": "#C47F17",
+
+  "red-500": "#E25858",
+
+  "green-500": "#168821",
+  "green-100": "#E3F5E1",
+
+  "container-size": "75vw",
+
+  "title-sm": "1.5rem",
+  "title-lg": "2.5rem",
+  "body-xsm": "0.75rem",
+  "body-md": "1rem",
+};
