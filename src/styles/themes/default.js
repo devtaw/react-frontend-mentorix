648d6ffddc938ad863fe7d4efa081cfb49d8a31a
Taw Simões <tawm.dev@gmail.com>
2023-10-23 06:27:36 -0300

    :bug: fix: Ajusta valores do tema do projeto

--- src/styles/themes/default.js
+++ src/styles/themes/default.js
@@ -1,58 +1,90 @@
 export const defaultTheme = {
   white: "#FFFFFF",
-  background: " #FAFAFA",
+  black: "#000000",
 
-  "gray-100": "#F5F5F5",
-  "gray-300": "#E0E0E0",
-  "gray-500": "#9E9E9E",
-  "gray-700": "#616161",
-  "gray-900": "#212121",
+  "background-light": "#E1F5FE",
+  "background-dark": "#01151E",
 
-  "primary-50": "#A863A5",
-  "primary-100": "#B467CB",
-  "primary-200": "#AF58DA",
-  "primary-300": "#AF31EA",
-  "primary-400": "#9A1FDD",
-  "primary-500": "#8211BB",
-  "primary-600": "#8747B3",
-  "primary-700": "#5E256F",
-  "primary-800": "#57115D",
-  "primary-900": "#471E5A",
+  // "title-color": "#",
+  // "text-color": "#",
 
-  "accent-50": "#F5F966",
-  "accent-100": "#FFF001",
-  "accent-200": "#FFDF00",
-  "accent-300": "#FFD300",
-  "accent-400": "#FDD53C",
-  "accent-500": "#FFCB05",
-  "accent-600": "#E1CC0F",
-  "accent-700": "#D3A54D",
+  "blue-gray-50": "#ECEFF1",
+  "blue-gray-100": "#CFD8DC",
+  "blue-gray-200": "#B0BEC5",
+  "blue-gray-300": "#90A4AE",
+  "blue-gray-400": "#78909C",
+  "blue-gray-500": "#607D8B",
+  "blue-gray-600": "#546E7A",
+  "blue-gray-700": "#455A64",
+  "blue-gray-800": "#37474F",
+  "blue-gray-900": "#263238",
 
-  "blue-50": "#A2A2FA",
-  "blue-100": "#A8D2DD",
-  "blue-200": "#7AD2FF",
-  "blue-300": "#02C2F4",
-  "blue-400": "#50BBF1",
-  "blue-500": "#29AAE1",
-  "blue-600": "#0397E8",
-  "blue-700": "#1E56AD",
-  "blue-800": "#0009C9",
+  "primary-100": "#B3E5FC",
+  "primary-200": "#81D4FA",
+  "primary-300": "#4FC3F7",
+  "primary-400": "#29B6F6",
+  "primary-500": "#03A9F4",
+  "primary-600": "#039BE5",
+  "primary-700": "#0288D1",
+  "primary-800": "#0277BD",
+  "primary-900": "#01579B",
+  "primary-a100": "#80D8FF",
+  "primary-a200": "#40C4FF",
+  "primary-a400": "#00B0FF",
+  "primary-a700": "#0091EA",
 
-  "red-500": "#E25A5A",
+  "secondary-50": "#EDE7F6",
+  "secondary-100": "#D1C4E9",
+  "secondary-200": "#B39DDB",
+  "secondary-300": "#9575CD",
+  "secondary-400": "#7E57C2",
+  "secondary-500": "#673AB7",
+  "secondary-600": "#5E35B1",
+  "secondary-700": "#512DA8",
+  "secondary-800": "#4527A0",
+  "secondary-900": "#311B92",
+  "secondary-a100": "#B388FF",
+  "secondary-a200": "#7C4DFF",
+  "secondary-a400": "#651FFF",
+  "primary-a700": "#6200EA",
 
-  "green-500": "#168822",
-  "green-100": "#E2F5E0",
+  "terciary-50": "#FFFDE7",
+  "terciary-100": "#FFF9C4",
+  "terciary-200": "#FFF59D",
+  "terciary-300": "#FFF176",
+  "terciary-400": "#FFEE58",
+  "terciary-500": "#FFEB3B",
+  "terciary-600": "#FDD835",
+  "terciary-700": "#FBC02D",
+  "terciary-800": "#F9A825",
+  "terciary-a100": "#FFFF8D",
+  "terciary-a200": "#FFFF00",
+  "terciary-a400": "#FFEA00",
+  "terciary-a700": "#FFD600",
+
+  //Cores semânticas ou cores de feedback:
+  "green-500": "#43A047", //success text
+  "orange-500": "#FF9800", //warning text
+  "red-600": "#D32F2F", //danger text
+  "blue-a400": "#2979ff", //info text
 
   "container-size": "75vw",
 
-  "title-xxlg": "2rem",
-  "title-xlg": "1.5rem",
-  "title-lg": "1.25rem",
-  "title-md": "1rem",
-  "title-sm": "0.875rem",
-  "title-xsm": "0.75rem",
+  //títulos:
+  "title-xxlg": "2rem", //h1-32px - Extra, Extra Grande
+  "title-xlg": "1.5rem", //h2-24px - Extra Grande
+  "title-lg": "1.25rem", //h3-20px - Grande
+  "title-md": "1rem", //h4-16px - Médio
+  "title-sm": "0.875rem", //h5-14px - Pequeno
+  "title-xsm": "0.75rem", //h6-12px - Extra Pequeno
 
-  "body-xsm": "0.75rem",
-  "body-sm": "0.875rem",
-  "body-md": "1rem",
+  //corpo de texto:
+  "body-xxsm": "0.64rem", //p-10px - Extra, Extra Pequeno
+  "body-xsm": "0.8rem", //p-12px - Extra Pequeno
+  "body-sm": "1rem", //p-16px - Pequeno
+  "body-md": "1.25rem", //p-20px - Médio
+  "body-mlg": "1.563rem", //p-25px - Médio Grande
+  "body-lg": "1.953rem", //p-32px - Grande
+  "body-xlg": "2.441rem", //p-40px - Extra Grande
+  "body-xxlg": "3.052rem", //p-48px - Extra, Extra Grande
 };
