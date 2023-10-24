8f6c9bb5e66ec0da405672d4001fa4fe515665a4
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-10-24 16:49:18 -0300

    :bug:	fix:  retira os Buttons que não são necessários na SectionAbout

--- src/pages/Home/SectionAbout/SectionAbout.jsx
+++ src/pages/Home/SectionAbout/SectionAbout.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { AboutSection } from "./SectionAbout.styled";
-import { Button } from "../../common/Button/Button";
 import { TextH1, TextH2 } from "../typography";
 
 function SectionAbout() {
   return (
     <AboutSection>
-      <TextH1>SOBRE A MENTORIX</TextH1>
+      <TextH1>SOBRE</TextH1>
       <TextH2>
         A Mentorix é uma plataforma de mentoria on-line dedicada a contribuir
         para o desenvolvimento de pessoas e carreiras em um ambiente
@@ -14,22 +13,6 @@ function SectionAbout() {
         e experiências entre profissionais, oferecendo uma experiência única que
         incentiva o autoconhecimento e o networking.
       </TextH2>
-      <ul>
-        <li>
-          <Button
-            texto="CADASTRE-SE AGORA"
-            variant="primary"
-            onClick={handleClick}
-          />
-        </li>
-        <li>
-          <Button
-            texto="LISTA DE MENTORES"
-            variant="primary"
-            onClick={handleClick}
-          />
-        </li>
-      </ul>
     </AboutSection>
   );
 }
