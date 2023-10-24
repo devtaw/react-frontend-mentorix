f59174020dca036aad90769c4952fb2a7c40b1b5
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-10-24 16:28:58 -0300

    :sparkles:	feat: Ajuste  na estilização sectionAbout

--- src/pages/Home/SectionAbout/SectionAbout.styled.js
+++ src/pages/Home/SectionAbout/SectionAbout.styled.js
@@ -5,16 +5,3 @@ const AboutSection = styled.section`
   padding: 40px;
   text-align: center;
 `;
-
-const AboutTitle = styled.h2`
-  font-size: 28px;
-  font-weight: 700;
-  color: ${(p) => p.theme["primary"]};
-  margin-bottom: 20px;
-`;
-
-const AboutDescription = styled.p`
-  font-size: 18px;
-  line-height: 1.5;
-  /* color: #333; */
-`;
