54382d0ae722ce9e5aa4259b53eb9675448f8b0d
Hellraiser997 <felipesg03@gmail.com>
2023-10-24 20:43:21 -0300

    :sparkles: feat: adicionado botão para abrir modal

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import { ContentContainer } from "../../common/ContentContainer.styled";
 import { TextH1 } from "../../common/typography";
 import Modal from "../../Components/Modal";
+import { useState } from "react";
 
 
 export function Cadastro() {
+  const [abreModal, setAbreModal] = useState(false)
+
+
   return (
     <ContentContainer>
       <div>
       <TextH1 color="secondary-700" variant="title-xsm">Cadastro</TextH1>
       <div>
-        <button onClick={() => {}}>Abre Modal</button>
+        <button onClick={() => setAbreModal(true)}>Abre Modal</button>
       </div>
-        <Modal />
+        <Modal isOpen={abreModal}/>
       </div>
     </ContentContainer>
   );
