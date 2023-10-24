b0e3c6552544323f571d1126efd0b569caefb1e2
Taw Simões <tawm.dev@gmail.com>
2023-10-24 03:39:15 -0300

    :lipstick: feat: Estiliza o título e a descrição da seção

--- src/pages/ListaMentores/ListaMentores.jsx
+++ src/pages/ListaMentores/ListaMentores.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { ContentContainer } from "../../common/ContentContainer.styled";
-import { ContentSection, SpanTitle } from "./ListaMentores.styled";
+import { ContentSection, SpanTitle, TextDescription, Title } from "./ListaMentores.styled";
 
 export function ListaMentores() {
   return (
-    <ContentContainer className="text-center">
+    <ContentContainer>
       <ContentSection>
         <SpanTitle>Expert Team</SpanTitle>
-        <h1>Lista de Mentores</h1>
+        <Title>Encontre o melhor online mentor para você</Title>
+        <TextDescription>Você não precisa lutar sozinho, você conta com nossa assistência e ajuda.</TextDescription>
       </ContentSection>
     </ContentContainer>
   );
--- src/pages/ListaMentores/ListaMentores.styled.js
+++ src/pages/ListaMentores/ListaMentores.styled.js
@@ -12,5 +12,32 @@ export const SpanTitle = styled.span`
   text-transform: uppercase;
   font-size: 0.875rem;
   line-height: 19px;
+  margin-left: 2px;
   color: ${(props) => props.theme["primary-900"]};
+`;
+
+export const Title = styled.h1`
+  font-family: "Open Sans", sans-serif;
+  font-size: 2.5rem;
+  font-weight: 700;
+  line-height: 1.2;
+  letter-spacing: -1px;
+  margin-top: 10px;
+  z-index: 1;
+  margin-bottom: 3px;
+  color: ${(props) => props.theme["blue-gray-900"]};
+  width: 493px;
+  text-align: left;
+  text-transform: capitalize;
+`;
+
+export const TextDescription = styled.p`
+  font-family: "Montserrat", sans-serif;
+  font-size: ${(props) => props.theme["body-sm"]};
+  color: ${(props) => props.theme["blue-gray-800"]};
+  font-weight: 400;
+  margin-bottom: 15px;
+  line-height: 26px;
+  margin-top: 0;
+  margin-left: 4px;
 `;
