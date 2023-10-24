8fc2bed4a7b2ce0d304aaca340bc491fb03b999b
Hellraiser997 <felipesg03@gmail.com>
2023-10-24 00:11:47 -0300

    :sparkles: Feat: criação de pasta Form para formuário de cadastro

---
+++ src/pages/Cadastro/Form/Form.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { GlobalStyle } from "../../styles/GlobalStyle";
+import { defaultTheme } from "styled-components";
+
+export default function Form() {
+  return (
+    <div>
+      <InputGroup className="mb-3">
+    <InputGroup.Text id="inputGroup-sizing-default" className="text-white">Nome - </InputGroup.Text>
+    <Form.Control
+      placeholder="Nome do usuário"
+      aria-label="Username"
+      aria-describedby="inputGroup-sizing-default"
+    />
+  </InputGroup>
+
+  <InputGroup className="mb-3">
+    <Form.Control
+      placeholder="Recipient's username"
+      aria-label="Recipient's username"
+      aria-describedby="basic-addon2"
+    />
+    <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+  </InputGroup>
+
+  <InputGroup className="mb-3">
+    <InputGroup.Text>$</InputGroup.Text>
+    <Form.Control aria-label="Amount (to the nearest dollar)" />
+    <InputGroup.Text>.00</InputGroup.Text>
+  </InputGroup>
+
+  <InputGroup>
+    <InputGroup.Text>With textarea</InputGroup.Text>
+    <Form.Control as="textarea" aria-label="With textarea" />
+  </InputGroup>
+    </div>
+  )
+}
