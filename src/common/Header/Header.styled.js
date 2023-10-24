4b678622f97ab2ec2f5565e6b4ac338cbab873d2
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-10-24 14:16:36 -0300

    :sparkles:	feat: ajuste nome StyledHeader

--- src/common/Header/Header.styled.js
+++ src/common/Header/Header.styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyleHeader1 = styled.header`
+export const StyleHeader = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
