cee6d5e913fb5ac0aa02803d408f8d0ef5fbdc1b
Hellraiser997 <felipesg03@gmail.com>
2023-10-24 20:42:59 -0300

    :sparkles: feat: criado modal para cadastro

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
+const STYLE_BACKGROUND = {
+    top: '0',
+    bottom: '0',
+    left: '0',
+    right: '0',
+    position: 'fixed',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    zIndex: '1000'
+}
+
+const STYLE_MODAL = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    padding: '250px',
+    backgroundColor: '#fff',
+    borderRadius: '12px'
+}
+
 export default function Modal({isOpen}) {
+
   
   if(isOpen) {
-      return <div>Modal</div>  
+      return(
+          <div style={STYLE_BACKGROUND}>
+            <div style={STYLE_MODAL}>Modal</div>
+            </div>  
+      )
   }
   return null
 }
