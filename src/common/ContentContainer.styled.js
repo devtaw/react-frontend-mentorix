a66a7c42031b8e38f30df8a1b134dc77fc4b42b3
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-10-25 14:22:13 -0300

    :sparkles:	feat: adiciona Button a pagina Footer

--- src/common/ContentContainer.styled.js
+++ src/common/ContentContainer.styled.js
@@ -2,5 +2,9 @@ import styled from "styled-components";
 
 export const ContentContainer = styled.div`
   margin: 0 auto;
+<<<<<<< Updated upstream
   width: 69.375vw;
+=======
+  width: 100%vw;
+>>>>>>> Stashed changes
 `;
