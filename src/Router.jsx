10a02f380636024bb9dfece231d8429be6e501f2
Hellraiser997 <felipesg03@gmail.com>
2023-10-25 13:42:29 -0300

    :fire: Remoção da rota login

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
-import { Login } from "./pages/Login/Login";
 import { Cadastro } from "./pages/Cadastro/Cadastro";
 import { ListaMentores } from "./pages/ListaMentores/ListaMentores";
 import { AreaMentor } from "./pages/AreaMentor/AreaMentor";
@@ -23,7 +22,6 @@ export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
       <Route path="/cadastro" element={<Cadastro />} />
       <Route path="/lista-mentores" element={<ListaMentores />} />
       <Route path="/area-mentor" element={<AreaMentor />} />
