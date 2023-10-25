2cae003654479f10e9df6fe3f6620e545db0a5b4
Hellraiser997 <felipesg03@gmail.com>
2023-10-25 13:34:37 -0300

    :sparkles: feat: importação criação de funções para abertura e fechamento do modal, importação de modal de login

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { ContentContainer } from "../../common/ContentContainer.styled";
 import Footer from "../../common/Footer/Footer";
+import { LoginModal } from "../Login/Login";
+import { useState } from "react";
 
 export function Home() {
+
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+   function handleCloseModal () {
+       setModalIsOpen(false)
+     }
+
+     function handleOpenModal () {
+      setModalIsOpen(true)
+     }
   return (
     <>
+    <button onClick={handleOpenModal}>
+      Abrir Modal (remover quando tiver o header)
+    </button>
       <ContentContainer>
         <h1>Home</h1>
       </ContentContainer>
       <Footer />
+      <LoginModal isOpen={modalIsOpen} onClose={handleCloseModal}/>
     </>
   );
 }
