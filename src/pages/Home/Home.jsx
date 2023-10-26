7567d9aa2f59b63507ba9da3c09eaa6141b310c7
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-10-26 02:56:22 -0300

    :sparkles:	feat:  adiciona importações

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { ContentContainer } from "../../common/ContentContainer.styled";
 import Footer from "../../common/Footer/Footer";
+import Header from "../../common/Header/Header";
+import SectionAbout from "./SectionAbout/SectionAbout";
+import { SectionContact } from "./SectionContact/SectionContact";
+import SectionWelcome from "./SectionWelcome/SectionWelcome";
+import SectionCadastro from "./SectionCadastro/SectionCadastro";
 
 export function Home() {
   return (
     <>
+      <Header />
+
       <ContentContainer>
-        <h1>Home</h1>
+        <SectionWelcome />
+        <SectionCadastro />
+        <SectionAbout />
+        <SectionContact />
       </ContentContainer>
+
       <Footer />
     </>
   );
