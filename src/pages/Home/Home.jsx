da17db4204e732a568125a4e3b77ee9e1712ee65
Hellraiser997 <felipesg03@gmail.com>
2023-10-25 22:40:32 -0300

    :sparkles: importação dos modais e função de redirecionamento do login

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,15 @@ import { ContentContainer } from "../../common/ContentContainer.styled";
 import Footer from "../../common/Footer/Footer";
 import { LoginModal } from "../Login/Login";
 import { useState } from "react";
+import { CadastroModal } from "../Cadastro/Cadastro";
+import { useNavigate } from "react-router-dom";
 
-export function Home() {
+export function Home({usuario, senha, setUsuario, setSenha}) {
 
   const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [cadastroIsOpen, setCadastroIsOpen] = useState(false)
+
+  const navigate = useNavigate();
 
    function handleCloseModal () {
        setModalIsOpen(false)
@@ -15,16 +20,33 @@ export function Home() {
      function handleOpenModal () {
       setModalIsOpen(true)
      }
+
+    function handleCadastroCloseModal () {
+      setCadastroIsOpen(false)
+    }
+
+    function handleCadastroOpenModal () {
+      setCadastroIsOpen(true)
+    }
+
+    function handleLogin () {
+      navigate("/area-mentor")
+      console.log(usuario, senha);
+  }
   return (
     <>
     <button onClick={handleOpenModal}>
       Abrir Modal (remover quando tiver o header)
     </button>
+    <button onClick={handleCadastroOpenModal}>
+      Abrir cadastro (remover quando tiver o header)
+    </button>
       <ContentContainer>
         <h1>Home</h1>
       </ContentContainer>
       <Footer />
-      <LoginModal isOpen={modalIsOpen} onClose={handleCloseModal}/>
+      <LoginModal isOpen={modalIsOpen} onClose={handleCloseModal} onSave={handleLogin}/>
+      <CadastroModal isOpen={cadastroIsOpen} onClose={handleCadastroCloseModal}/>
     </>
   );
 }
