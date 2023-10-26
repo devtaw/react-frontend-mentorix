06101d4f05578d562962d2e9a22be1d747262caf
Taw Simões <tawm.dev@gmail.com>
2023-10-26 03:52:22 -0300

    :lipstick: feat: ajuste no container geral

--- src/common/ContentContainer.styled.js
+++ src/common/ContentContainer.styled.js
@@ -2,9 +2,5 @@ import styled from "styled-components";
 
 export const ContentContainer = styled.div`
   margin: 0 auto;
-<<<<<<< Updated upstream
-  width: 69.375vw;
-=======
-  width: 100%vw;
->>>>>>> Stashed changes
+  width: 100vw;
 `;
