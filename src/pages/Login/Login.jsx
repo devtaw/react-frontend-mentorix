7c127760f53c21785bed8c175209ad9802d4368d
Hellraiser997 <felipesg03@gmail.com>
2023-10-25 22:38:50 -0300

    :sparkles: feat: criada função para redirecionamento, criados estados para integração com BD

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { MentorixModal } from "../../common/MentorixModal/MentorixModal";
 import { useState } from "react";
-import Form from "../Cadastro/FormCadastro/FormCadastro";
+import FormLogin from "./FormLogin/FormLogin";
+import { useNavigate } from "react-router-dom";
 
 
-export function LoginModal({isOpen, onClose}) {
+export function LoginModal({isOpen, onClose, onSave}) {
 
   const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [usuario, setUsuario] = useState('')
+  const [senha, setSenha] = useState('')
+
+  const navigate = useNavigate();
+
+    function handleLogin () {
+        navigate("/area-mentor")
+        console.log(usuario, senha);
+    }
 
   // function handleCloseModal () {
   //   setModalIsOpen(false)
   // }
   return (
-    <MentorixModal isOpen={isOpen} title='Login' buttonSaveName="Entrar" onClose={onClose}> 
-      <Form />
+    <MentorixModal isOpen={isOpen} title='Login' buttonSaveName="Entrar" onClose={onClose} onSave={handleLogin}> 
+      <FormLogin />
     </MentorixModal>
     
   );
