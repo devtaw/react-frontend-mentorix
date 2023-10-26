58dd5d025e0737bdea234f9c74ad244aa56f97d1
Hellraiser997 <felipesg03@gmail.com>
2023-10-26 02:34:02 -0300

    :sparkles: feat: adicionando lógica de validação e labels

--- src/pages/Login/FormLogin/FormLogin.jsx
+++ src/pages/Login/FormLogin/FormLogin.jsx
@@ -1,43 +1,67 @@
-import { Password, User } from '@phosphor-icons/react';
-import React from 'react'
+import { Envelope, Lock} from '@phosphor-icons/react';
+import React, { useState } from 'react'
 import { InputGroup, Form } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
 // import { User } from "@phosphor-icons/react";
 
 export default function FormLogin({usuario, setUsuario, senha, setSenha}) {
 
+    // const [email, setEmail] = useState('')
+    // const [senha, setSenha] = useState('')
+
+    function handleLogin(e) {
+        e.preventDefault();
+
+        if(email.length == 0 || email.indexOf('@') === -1) {
+            alert('E-mail inválido');
+            return;
+        }
+
+        if (senha.length < 5) {
+            alert('E-mail inválido');
+            return;
+        }
+
+        navigate("/area-mentor");
+
+    }
+
+
     const navigate = useNavigate();
 
-    function handleLogin () {
-        navigate("/area-mentor");
-    }
+    // function handleLogin () {
+    //     navigate("/area-mentor");
+    // }
 
 
 
   return (
 
     <div>
+        <label>E-mail</label>
       <InputGroup className="mb-3">
     <InputGroup.Text id="inputGroup-sizing-default" className="text-white">
-    <User size={16} color="#4d0057" weight="fill" />
+    <Envelope size={16} color="#673AB7" weight="fill" />
     </InputGroup.Text>
     <Form.Control
-      placeholder="Nome do usuário"
-      aria-label="Username"
-      aria-describedby="inputGroup-sizing-default"      
-      onChange={(e) => setUsuario(e.target.value)}
+      placeholder="E-mail"
+      aria-label="Email"
+      aria-describedby="inputGroup-sizing-default"   
+      type='email'
+    //   onChange={(e) => setUsuario(e.target.value)}
     />
   </InputGroup>
-
+    <label>Senha</label>
   <InputGroup className="mb-3">
     <InputGroup.Text id="inputGroup-sizing-default" className="text-white">
-    <Password size={16} color="#4d0057" weight="fill" />
+    <Lock size={16} color="#673AB7" weight="fill" />
     </InputGroup.Text>
     <Form.Control
       placeholder="Senha"
       aria-label="Password"
       aria-describedby="inputGroup-sizing-default"
-      onChange={(e) => setSenha(e.target.value)}
+      type='password'
+    //   onChange={(e) => setSenha(e.target.value)}
     />
   </InputGroup>
 
