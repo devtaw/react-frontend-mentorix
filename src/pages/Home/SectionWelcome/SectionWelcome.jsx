133d9242d1dc37550aa2b596642d26084de60234
Taw Simões <tawm.dev@gmail.com>
2023-10-28 17:02:46 -0300

    :mag: Altera conteúdo da Section Bem Vindo

--- src/pages/Home/SectionWelcome/SectionWelcome.jsx
+++ src/pages/Home/SectionWelcome/SectionWelcome.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  WelcomeContainer,
-  TextContainer,
-  ImageContainer,
-  Container,
-} from "./SectionWelcome.styled";
+import { WelcomeContainer, TextContainer, ImageContainer, Container } from "./SectionWelcome.styled";
 import { TextH1, TextH2, TextH6 } from "../../../common/typography";
 import { useTheme } from "styled-components";
 import { MentorixButton } from "../../../common/Button/MentorixButton.styled";
@@ -19,27 +14,17 @@ function SectionWelcome() {
       <Container>
         <TextContainer>
           <TextH6>MENTORES ESPECIALIZADOS </TextH6>
-          <TextH1 variant={"title-xxlg"}>
-            Quer mudar de vida? Escolha Mentorix!
-          </TextH1>
+          <TextH1 variant={"title-xxlg"}>Mude sua vida! Escolha Mentorix</TextH1>
           <TextH2 variant={"body-xsm"}>
-            Transforme sua carreira com nossa plataforma de mentoria.
-            Conhecimento compartilhado, futuro assegurado. Junte-se a nós.
+            Transforme sua carreira com nossa plataforma de mentoria. Conhecimento compartilhado, futuro assegurado.
           </TextH2>
 
-          <MentorixButton
-            color={"terciary-200"}
-            onClick={handleClick}
-            className="mt-4"
-          >
+          <MentorixButton color={"terciary-200"} onClick={handleClick} className="mt-4">
             Conheça os Mentores
           </MentorixButton>
         </TextContainer>
         <ImageContainer>
-          <img
-            src={image}
-            alt="Imagem de seis pessoas, para referenciar os Mentores."
-          />
+          <img src={image} alt="Imagem de seis pessoas, para referenciar os Mentores." />
         </ImageContainer>
       </Container>
     </WelcomeContainer>
