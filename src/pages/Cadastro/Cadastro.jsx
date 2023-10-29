17bc2de99ba3a238487a2ce3407ad4b9801a9095
Hellraiser997 <felipesg03@gmail.com>
2023-10-29 16:42:16 -0300

    :sparkles: try/catch para tratativa de erros do cadastro

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -6,33 +6,20 @@ import { useNavigate } from "react-router";
 import { useSnackbar } from "notistack";
 
 export function Cadastro() {
-  const navigate = useNavigate();
-  const snackbar = useSnackbar();
   const handleCadastro = async () => {
     e.preventDefault();
-
-    const body = {
-      nomeCompleto,
-      email,
-      senha,
-      confirmaSenha,
-      fotoPerfil,
-      biografia,
-      linkedin,
-      profissao,
-    };
+    const navigate = useNavigate();
+    const snackbar = useSnackbar();
 
     try {
-      if (senha === confirmaSenha) {
-        const resposta = await postMentores(body);
-        if (resposta) {
-          // todo: chamar notificação de sucesso, redirecionar para área mentor
-          snackbar.enqueueSnackbar("Usuário cadastrado com sucesso!", {
-            variant: "success",
-          });
-          navigate("/area-mentor");
-          return;
-        }
+      const resposta = await postMentores(body);
+      if (resposta) {
+        // todo: chamar notificação de sucesso, redirecionar para área mentor
+        snackbar.enqueueSnackbar("Usuário cadastrado com sucesso!", {
+          variant: "success",
+        });
+        navigate("/area-mentor");
+        return;
       }
     } catch (error) {
       // todo: chamar notificação de erro
@@ -40,6 +27,17 @@ export function Cadastro() {
         variant: "error",
       });
     }
+  };
+
+  const body = {
+    nomeCompleto,
+    email,
+    senha,
+    confirmaSenha,
+    fotoPerfil,
+    biografia,
+    linkedin,
+    profissao,
   };
 
   return (
