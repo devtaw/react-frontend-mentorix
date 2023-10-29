b759b58865fd63f8f373078cd11797c7c412a2b6
Hellraiser997 <felipesg03@gmail.com>
2023-10-29 18:01:09 -0300

    :fire: remoção de const config

--- src/common/services/MentorService.js
+++ src/common/services/MentorService.js
@@ -4,10 +4,7 @@ export async function getMentores() {
   return httpClient.get("/mentores").then((response) => response.data);
 }
 
-export async function postMentores(body, senha) {
-  const config = {
-
-  }
-  const resposta = await httpClient.post('/mentores', body, config)
+export async function postMentores(body) {
+  const resposta = await httpClient.post('/mentores', body)
   return resposta.data
 }
