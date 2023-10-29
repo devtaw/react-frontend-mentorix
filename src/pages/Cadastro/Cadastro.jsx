ceeb89c8801339570c3f921abf8d8ed1d06cef26
Hellraiser997 <felipesg03@gmail.com>
2023-10-29 16:03:46 -0300

    :sparkles: feat: adicionado try/catch para tratamento de erros

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { useState } from "react";
-import { MentorixModal } from "../../common/MentorixModal/MentorixModal";
 import FormCadastro from "./FormCadastro/FormCadastro";
 import Header from "../../common/Header/Header";
 import Footer from "../../common/Footer/Footer";
 import { useNavigate } from "react-router";
 import { useSnackbar } from "notistack";
 
-export function Cadastro({ isOpen, onClose }) {
+export function Cadastro() {
   const navigate = useNavigate();
   const snackbar = useSnackbar();
   const handleCadastro = async () => {
@@ -26,18 +24,21 @@ export function Cadastro({ isOpen, onClose }) {
 
     if (senha === confirmaSenha) {
       const resposta = await postMentores(body);
-      if (resposta) {
-        // todo: chamar notificação de sucesso, redirecionar para área mentor
-        snackbar.enqueueSnackbar("Usuário cadastrado com sucesso!", {
-          variant: "success",
+      try {
+        if (resposta) {
+          // todo: chamar notificação de sucesso, redirecionar para área mentor
+          snackbar.enqueueSnackbar("Usuário cadastrado com sucesso!", {
+            variant: "success",
+          });
+          navigate("/area-mentor");
+          return;
+        }
+      } catch (error) {
+        // todo: chamar notificação de erro
+        snackbar.enqueueSnackbar("Erro ao cadastrar usuário!", {
+          variant: "error",
         });
-        navigate("/area-mentor");
-        return;
       }
-      // todo: chamar notificação de erro
-      snackbar.enqueueSnackbar("Erro ao cadastrar usuário!", {
-        variant: "error",
-      });
     }
   };
 
