f5466a7d7bc5a2460cc421924341d64e8bde41cb
Jéssica Hickembick <j.hickembick@gmail.com>
2023-10-29 20:04:10 -0300

    :sparkles: feat: craiçao das rotas do back da area mentor

--- src/common/services/MentorService.js
+++ src/common/services/MentorService.js
@@ -1,5 +1,22 @@
 import { httpClient } from "../../core/httpClient";
+import axios from "axios"; 
 
-export async function getMentores() {
+export  function getMentores() {
   return httpClient.get("/mentores").then((response) => response.data);
 }
+
+export  function getMentorById(id) {
+  console.log(id);
+  return httpClient.get("/mentores/" + id).then((response) => response.data);
+}
+
+export function updateMentor(id, mentor) {
+  const url = `/mentores/${id}`;
+  return axios.put(url, mentor)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error; 
+    });
+}
+
+
