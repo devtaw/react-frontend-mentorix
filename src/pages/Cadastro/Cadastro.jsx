c6f7878c12af4951e22984fbca3502aefa01cf69
Hellraiser997 <felipesg03@gmail.com>
2023-10-29 15:14:37 -0300

    :sparkles: feat: criada lógica para consumo de endpoint

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -6,24 +6,30 @@ import Header from "../../common/Header/Header";
 import Footer from "../../common/Footer/Footer";
 
 export function Cadastro({ isOpen, onClose }) {
-  // const handleCadastro = async () => {
-  //   e.preventDefault()
+  const handleCadastro = async () => {
+    e.preventDefault();
 
-  //   const body = {
-  //     nomeCompleto,
-  //     email,
-  //     senha,
-  //     confirmaSenha,
-  //     fotoPerfil,
-  //     biografia,
-  //     linkedin,
-  //     profissao
-  //   }
+    const body = {
+      nomeCompleto,
+      email,
+      senha,
+      confirmaSenha,
+      fotoPerfil,
+      biografia,
+      linkedin,
+      profissao,
+    };
 
-  //   if (senha === confirmaSenha) {
-  //     const resposta = await postMentores(body)
-  //   }
-  // }
+    if (senha === confirmaSenha) {
+      const resposta = await postMentores(body);
+      if (resposta) {
+        // todo: chamar notificação de sucesso, redirecionar para área mentor
+
+        return;
+      }
+      // todo: chamar notificação de erro
+    }
+  };
 
   return (
     <div>
