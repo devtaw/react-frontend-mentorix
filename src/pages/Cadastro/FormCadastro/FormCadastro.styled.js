ae2a2e8cef2fd89e83d7ce3b99acf75bddb94136
Taw Simões <tawm.dev@gmail.com>
2023-10-29 16:14:44 -0300

    :bulb: docs: Ajusta cor mencionada no comentário do Form de Cadastro

--- src/pages/Cadastro/FormCadastro/FormCadastro.styled.js
+++ src/pages/Cadastro/FormCadastro/FormCadastro.styled.js
@@ -1,4 +1,4 @@
 // const InputGroup.Text = styled.inputgroup.text`
-//     background-color: ${(p) => p.theme["secondary-900"]};
-//     color: 
+//     background-color: ${(p) => p.theme["background-dark"]};
+//     color:
 // `
