08143776c6eba26c5a3d06eb8dbc4ad11b1f637d
Hellraiser997 <felipesg03@gmail.com>
2023-10-29 15:12:35 -0300

    :sparkles: criada rota post para cadastro

--- src/common/services/MentorService.js
+++ src/common/services/MentorService.js
@@ -3,3 +3,11 @@ import { httpClient } from "../../core/httpClient";
 export async function getMentores() {
   return httpClient.get("/mentores").then((response) => response.data);
 }
+
+export async function postMentores(body, senha) {
+  const config = {
+
+  }
+  const resposta = await httpClient.post('/mentores', body, config)
+  return resposta.data
+}
