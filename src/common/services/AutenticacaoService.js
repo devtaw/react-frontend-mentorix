80651c55de5332507101513e7a5d2756c0fc0d1f
Hellraiser997 <felipesg03@gmail.com>
2023-10-30 00:56:18 -0300

    :bug: fix: alteração na requisição

--- src/common/services/AutenticacaoService.js
+++ src/common/services/AutenticacaoService.js
@@ -1,5 +1,25 @@
 import { httpClient } from "../../core/httpClient";
 
-export function login(email, senha) {
-    return httpClient.get('/login', { email, senha })
+export const login = async (email, senha) => {
+    try {
+        const config = {
+            headers: {
+                'X-password': senha
+            },
+        }
+        const resposta = await httpClient.get('/autenticacao', { email, senha })
+        return resposta.data
+
+    } catch (error) {
+        if(error.response) {
+            return {
+                message: error.response.data.message,
+                success: error.response.data.success
+            }
+        } else {
+            return {
+                message: 'erro inesperado',
+            }
+        }
+    }
 }
