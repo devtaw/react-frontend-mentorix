4a01f2ecd7129b4ca2ffb89dbb20da101a68dff6
Taw Simões <tawm.dev@gmail.com>
2023-10-30 00:53:17 -0300

    :lipstick: feat: Ajusta estilização da Header

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -5,6 +5,7 @@ import { useTheme } from "styled-components";
 import { MentorixButton } from "../Button/MentorixButton.styled.js";
 import { useState } from "react";
 import logoImg from "../../assets/img/logo-header.png";
+import { SignIn } from "@phosphor-icons/react";
 
 const Header = () => {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
@@ -30,42 +31,42 @@ const Header = () => {
         <a href="/">
           <img src={logoImg} alt="Logo Mentorix" />
         </a>
-        <nav>
+
+        <nav className="d-flex align-items-center">
           <ul className="lista_links m-0">
             <li>
               <Link to="/" className="link">
-                HOME
+                Home
               </Link>
             </li>
 
             <li>
               <a href="#about" className="link">
-                SOBRE
+                Sobre
               </a>
             </li>
             <li>
               <Link to="/lista-mentores" className="link">
-                MENTORES
+                Mentores
               </Link>
             </li>
             <li>
               <a href="#contact" className="link">
-                CONTATO
+                Contato
               </a>
-            </li>
-            <li>
-              <Link to="/login" className="link login-link">
-                LOGIN
-              </Link>
-            </li>
-
-            <li>
-              <MentorixButton color={"terciary-200"} onClick={handleClick}>
-                Criar conta
-              </MentorixButton>
             </li>
           </ul>
         </nav>
+        <div className="d-flex align-items-center">
+          <Link to="/login" className="link login-link d-flex align-items-center gap-1">
+            Acessar
+            <SignIn color="#311B92" weight="fill" size={27} />
+          </Link>
+
+          <MentorixButton color={"terciary-200"} onClick={handleClick}>
+            Cadastrar
+          </MentorixButton>
+        </div>
       </StyleHeader>
     </>
   );
