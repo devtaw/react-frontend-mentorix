0cb4ec2fdb2540710bbc5aac24cbf1611d35c693
Hellraiser997 <felipesg03@gmail.com>
2023-10-29 21:00:51 -0300

    :sparkles: style: estilização do formulário e button de cadastro

--- src/pages/Cadastro/FormCadastro/FormCadastro.styled.js
+++ src/pages/Cadastro/FormCadastro/FormCadastro.styled.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-margin-top: 10px;
-margin-bottom: 10px
+margin-top: 40px;
+margin-bottom: 40px;
+margin-left: 20%;
+width: 45rem;
+align-items: center;
+`
+
+export const Button = styled.div`
+margin-left: 73.6%;
 `
