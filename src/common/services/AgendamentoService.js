4e142e062f86e5213fc06263fa8c43d089f2de7f
Jéssica Hickembick <j.hickembick@gmail.com>
2023-10-30 02:00:08 -0300

    sparkles: feat: criaçaõ da rota put do aceiteagendamento

--- src/common/services/AgendamentoService.js
+++ src/common/services/AgendamentoService.js
@@ -3,3 +3,7 @@ import { httpClient } from "../../core/httpClient";
 export function postAgendamento(agendamento) {
   return httpClient.post("/agendamentos", agendamento).then((response) => response.data);
 }
+
+export function putAgendamento(idDoAgendamento, agendamento) {
+  return httpClient.put("/agendamentos/"+idDoAgendamento, agendamento).then((response) => response.data);
+}
