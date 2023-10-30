7ee2d689dd1d934a83ea314b8b8fe75a7e1808f7
Hellraiser997 <felipesg03@gmail.com>
2023-10-29 23:07:34 -0300

    :bug: fix: const resposta dentro do try

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,31 +1,38 @@
 import React from "react";
-import { MentorixModal } from "../../common/MentorixModal/MentorixModal";
-import { useState } from "react";
 import FormLogin from "./FormLogin/FormLogin";
 import { useNavigate } from "react-router-dom";
 import Header from "../../common/Header/Header";
 import Footer from "../../common/Footer/Footer";
+import { useSnackbar } from "notistack";
+import { login } from "../../common/services/AutenticacaoService";
 
-export function Login({ isOpen, onClose }) {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-
+export function Login({ email, senha }) {
   const navigate = useNavigate();
+  const snackbar = useSnackbar();
 
-  function handleLogin() {
-    navigate("/area-mentor");
-    console.log(usuario, senha);
-  }
+  const handleLogin = async () => {
+    try {
+      const resposta = await login(email, senha);
+      if (resposta) {
+        snackbar.enqueueSnackbar("Login efetuado com sucesso!", {
+          variant: "success",
+        });
+        navigate("/area-mentor");
+        return;
+      }
+    } catch (error) {
+      snackbar.enqueueSnackbar("Erro ao efetuar login!", {
+        variant: "error",
+      });
+      console.log(error);
+    }
+  };
 
-  // function handleCloseModal () {
-  //   setModalIsOpen(false)
-  // }
   return (
     <div>
       <Header />
-      <FormLogin />
-      <Footer/>
+      <FormLogin onSave={handleLogin} />
+      <Footer />
     </div>
   );
 }
